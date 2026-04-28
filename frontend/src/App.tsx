88140d002c6cabb8446a11d58cf06fcbe0754dc9
Vitor Torquato <vitor@digitaltrafficbrain.com>
2026-04-28 12:28:17 +0200

    redirected when signed in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,19 @@ import { SignUp } from './pages/SignUp/SignUp'
 import { Home } from './pages/Home/Home'
 import { CollectionSettings } from './pages/CollectionSettings/CollectionSettings'
 import { ProtectedRoute } from './components/ProtectedRoute'
+import { useAuth } from './context/AuthContext'
+
+function GuestRoute({ children }: { children: React.ReactNode }) {
+  const { isAuthenticated } = useAuth()
+  return isAuthenticated ? <Navigate to="/home" replace /> : <>{children}</>
+}
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/signin" replace />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="/signin" element={<GuestRoute><SignIn /></GuestRoute>} />
+      <Route path="/signup" element={<GuestRoute><SignUp /></GuestRoute>} />
       <Route path="/home" element={
         <ProtectedRoute><Home /></ProtectedRoute>
       } />
